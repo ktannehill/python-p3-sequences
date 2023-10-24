2d22bc604bfff6766ddc078d9a3b626ca5b0b2e8
Kat Tannehill <katetannehill@gmail.com>
2023-10-23 20:09:35 -0600

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        seq = [0, 1]
+
+        while len(seq) < length:
+            seq.append(seq[-1] + seq[-2])
+        print(seq)
     pass
+
+print_fibonacci(9)
